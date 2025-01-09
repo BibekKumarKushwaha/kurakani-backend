f5294f1579c3b65d34896aa234964ac5b567ffa7
BibekKumarKushwaha <bibekkumar345@gmail.com>
2025-01-09 18:02:43 +0545

    phone number

--- models/user.js
+++ models/user.js
@@ -1,3 +1,52 @@
+// import mongoose, { Schema, model } from "mongoose";
+// import { hash } from "bcrypt";
+
+// const schema = new Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//     bio: {
+//       type: String,
+//       required: true,
+//     },
+//     username: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//       select: false,
+//     },
+//     avatar: {
+//       public_id: {
+//         type: String,
+//         required: true,
+//       },
+//       url: {
+//         type: String,
+//         required: true,
+//       },
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// schema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
+
+//   this.password = await hash(this.password, 10);
+// });
+
+// export const User = mongoose.models.User || model("User", schema);
+
+
+
 import mongoose, { Schema, model } from "mongoose";
 import { hash } from "bcrypt";
 
@@ -26,10 +75,17 @@ const schema = new Schema(
         type: String,
         required: true,
       },
+      
+  
       url: {
         type: String,
         required: true,
       },
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+      unique: true,
     },
   },
   {
@@ -44,3 +100,4 @@ schema.pre("save", async function (next) {
 });
 
 export const User = mongoose.models.User || model("User", schema);
+
